3bfd1ada4836eb8a76d4fc81bfeb1dfb1d84c138
ManBaCao <2323332693@qq.com>
2019-09-03 23:12:22 +0800

    视图划分以及路由的基本配置

--- My_CNode/src/App.js
+++ My_CNode/src/App.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
+import RouterIndex from "./router/index"
 
 class App extends Component {
-    render() {
-        return (
-          <h1>
-            wode 
-          </h1>
-        )
+    render () {
+        return  <RouterIndex />
     }
 }
 
