c1d722bd03c7ae6d14b363c13a316e640330b311
ManBaCao <2323332693@qq.com>
2019-09-04 23:05:49 +0800

    引入antd

---
+++ My_CNode/src/view/main-header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Layout, Row, Col } from "antd";
+
+export default class MainHeader extends Component {
+    render () {
+        return (<Layout.Header>
+            <Row className="warp">
+                <Col md={6} xs={24}>
+                    <h1 id="logo">cNode</h1>
+                </Col>
+                <Col md={18} xs ={0}></Col>
+            </Row>
+        </Layout.Header>);
+    }
+}
