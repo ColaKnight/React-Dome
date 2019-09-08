3a4b579b363f4f29c685d0c0eea066c03c3b3477
ManBaCao <2323332693@qq.com>
2019-09-08 22:08:22 +0800

    详情

---
+++ My_CNode/src/view/details/txtDetails.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Card, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import TxtTag from "../txtTag";
+
+class TxtDetails extends Component {
+    render () {
+        let {loading, data} = this.props;
+        const title = (<div>
+            <h1>{data.title}</h1>
+            <div style={{
+                display:"flex",
+                alignItems:"center"
+            }}>
+                <TxtTag data={data.data}/>
+                <Avatar src={data.data.author.avatar_url}/>
+                <Link to={"/user/"+data.data.author.loginname}>
+                    {data.data.author.loginname}
+                </Link>
+                发表于:{data.data.create_at.split("T")[0]}
+            </div>
+        </div>);
+        return ( <Card
+            loading={loading}
+            title={title}
+        >
+            <div
+                dangerouslySetInnerHTML={{
+                    __html:data.data.content
+                }}
+            >
+            </div>
+        </Card>);
+    }
+}
