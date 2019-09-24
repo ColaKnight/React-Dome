7fe711802204cc3166b7ff16128bee13bdd9a292
ManBaCao <2323332693@qq.com>
2019-09-24 14:26:06 +0800

    redux

--- My_CNode/src/index.js
+++ My_CNode/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
     <BrowserRouter>
