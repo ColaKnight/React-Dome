a4eaff5bdee0ac32689cd64cd909c53a09a7ecb4
ManBaCao <2323332693@qq.com>
2019-09-25 21:22:05 +0800

    Update index.js

---
+++ My_CNode/src/view/reducers/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+
+let reducer = combineReducers({
+    list,
+    details,
+    user
+})
+
+class reduces extends Component {
+    render () {
+        return 
+    }
+}
+
+export default reduces
