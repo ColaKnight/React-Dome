2a6cf986bf33b1a56ae94721ea9faf4116c60025
ManBaCao <2323332693@qq.com>
2019-10-11 22:24:43 +0800

    事件绑定

--- demo1/src/Sister.js
+++ demo1/src/Sister.js
@@ -1,11 +1,25 @@
 import React, {Component, Fragment} from "react"
 
 class Sister extends Component{
+    constructor (props) {
+        super(props)
+        this.state = {
+            inputValue: "ManBa",
+            list: []
+        }
+    }
+
+    inputChange(e){
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
     render () {
         return (
             <Fragment>
                 <div>
-                    <input />
+                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)} />
                     <button>增加服务</button>
                 </div>
                 <ul>
