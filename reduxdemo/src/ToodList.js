ecbb5d85ee92a83b19f4113199fcc1997442f7cb
ManBaCao <2323332693@qq.com>
2019-10-22 23:23:30 +0800

    使用antd

--- reduxdemo/src/ToodList.js
+++ reduxdemo/src/ToodList.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { Input, Button, List } from 'antd';
+import "antd/dist/antd.css"
+
+const data = [
+    "篮球",
+    "足球",
+    "乒乓球"
+]
 
 class ToodList extends Component {
  
     render() { 
         return ( 
-            <div>
-                Hello world
+            <div style={{margin:"10px"}}>
+                <div>
+                    <Input 
+                        placeholder="Write Something"
+                        style={{width:"250px", marginRight:"10px"}}
+                    />
+                    <Button type="primary">增加</Button>
+                </div>
+                <div style={{margin:"10px", width:"300px"}}>
+                    <List
+                        bordered
+                        dataSource={data}
+                        renderItem={item=>(
+                            <List.Item>
+                                {item}
+                            </List.Item>
+                        )}
+                    ></List>
+                </div>
             </div>
          );
     }
