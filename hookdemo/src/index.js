beb81175a60a96000cfb3343bf061ecc745021d7
ManBaCao <2323332693@qq.com>
2019-12-21 22:40:55 +0800

    hooks

--- hookdemo/src/index.js
+++ hookdemo/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './example'
+import Example2 from './example2'
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(
+    <Example2 />, 
+    document.getElementById('root')
+);
 
